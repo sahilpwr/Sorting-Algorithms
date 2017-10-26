bda3eb08a6ed25c6ced29ca3f9d71b6c2895dbae
sahilpwr <sahil.sandypwr@gmail.com>
2017-10-26 16:06:59 -0400

    Insertion Sort

--- src/com/sort/InsertionSort.java
+++ src/com/sort/InsertionSort.java
@@ -1,15 +1,24 @@
 package com.sort;
 
+import java.util.Arrays;
+
 public class InsertionSort {
 	public void sort(int[] array)
 	{
 		int len=array.length;
-		
-		
-		for(int i=0;i<len;i++)
+		for(int i=1;i<len;i++)
 		{
-			
+			int j=i-1;
+			int key=array[i];
+			while(j>=0&&array[j]>key)
+			{
+				array[j+1]=array[j];
+				j--;
+			}
+			array[j+1]=key;
+				
 		}
+		System.out.println(Arrays.toString(array));
 	}
 	
 	public static void main(String[] args) {
